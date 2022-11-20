3152e9431bcf3ff97a40bc8a20cbd58be190145a
jackkelly247 <80363673+jackkelly247@users.noreply.github.com>
2022-11-20 04:13:14 +0000

    Add files via upload
    Updated function files. Now contains a function that can find if a note or set of notes is above a threshold or not within an audio clip and how long this note lasts for when STFT information is passed to it. This could then be used to create timing diagrams perhaps if some frequency was being seen to occur at too high a amplitude.

--- functions.py
+++ functions.py
@@ -43,18 +43,63 @@ def frequency_presence_detector(time_loop, freq_loop, amplitudes, time, freq, dB
 
 def frequency_time_detector(time_loop, freq_loop, amplitudes, time, freq, dB):
     notes = []
-    when = []
-    note = ['0']
+    timing_list = []
+    intermittent = []
+    first = 1
     for i in range (0, time_loop): #time loop
         for j in range (0,freq_loop):
-            if (amplitudes[j,i] > dB) and (librosa.hz_to_note(freq[j]) not in notes or time[i]not in when):
-                notes.append(librosa.hz_to_note(freq[j]))
-                when.append(time[i])
+            if amplitudes[j,i] > dB:
                 amplitudes[j,i] = 100
+                if librosa.hz_to_note(freq[j]) not in notes:
+                    notes.append(librosa.hz_to_note(freq[j]))
+                    timing_list.append([time[i]])
+                else:
+                    timing_list[notes.index(librosa.hz_to_note(freq[j]))].append(time[i])
             else:
                 amplitudes[j,i] = 0
-    for i in range(1,freq_loop):
-        note.append(librosa.hz_to_note(freq[i]))
-    plt.pcolormesh(time, freq, amplitudes)
+    #plotting
+    if 0:
+        plt.figure(figsize=(10,6))
+        plt.pcolormesh(time, freq, amplitudes)
+        plt.xlabel("Time (sec)"); plt.ylabel("frequency (Hz)")
+        plt.title('STFT Channel 1 - present frequencies')
+        plt.show()
+    # sub plots of timing graphs and longest time a note is present
+    if 1:
+        figure, axis = plt.subplots(4,1)
+        notes_time_period = []
+        for i in range(0,len(notes)):
+            num = 0
+            max_consec = 0
+            for j in range(0,len(time)):
+                if time[j] in timing_list[i]:
+                    intermittent.append(1+i)
+                    num = num + 1
+                    if num > max_consec:
+                        max_consec = num
+                        
+                else:
+                    intermittent.append(0)
+                    num = 0
+
+            time_period = (time[j] - time[j-max_consec])*1000
+            notes_time_period.append(time_period)
+            axis[int(i/3)].step(time, intermittent, label = notes[i])
+            intermittent.clear()
+        for ax in axis:
+            ax.set(xlabel= "time (sec)", ylabel = "present or not")
+            ax.legend(loc = "upper right")
+        print(notes, notes_time_period)
+    #plot one timing graph with dictionary value
+    if 0:
+        search_note = "E6"
+        for each_time in time:
+                if each_time in timing_list[notes.index(search_note)]:
+                    intermittent.append(1)
+                else:
+                    intermittent.append(0)
+        plt.step(time,intermittent)
+        plt.title("timing diagram for presence of - " + search_note)
+        plt.xlabel("Time (sec)"); plt.ylabel("Present or not")
     plt.show()
     return
